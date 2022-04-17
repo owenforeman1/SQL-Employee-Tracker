d23670811995c9f7ef6cbaa7c836f5a2933e19ea
Owen Foreman <owen4man@gmail.com>
2022-04-16 23:34:15 -0400

    seeds work

--- db/schema.sql
+++ db/schema.sql
@@ -5,14 +5,14 @@ USE employee_db;
 
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(30) NOT NULL,
+  name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL(30, 5) NOT NULL,
+  salary DECIMAL(30, 2) NOT NULL,
   department_id INT,
   PRIMARY KEY (id),
   FOREIGN KEY (department_id) 
@@ -25,8 +25,8 @@ CREATE TABLE employee (
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT,
+  manager_id INT, 
   PRIMARY KEY (id),
-  FOREIGN KEY (role_id) 
-  REFERENCES role(id) 
-  ON DELETE SET NULL
+  FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+  FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO department (name)
+VALUES ("Engineering"),
+       ("Sales"),
+       ("Finance"),
+       ("Legal");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Salesperson", "80000", 002),
+       ("Software Engineer", "120000.00", 001),
+       ("Senior Engineer", "150000.00", 001),
+       ("Lawyer", "190000.00", 004),
+       ("Account Manager", "160000.00", 003),
+       ("Accountant", "125000.00", 003),
+       ("Legal Team Lead", "250000.00", 004);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Owen", "Foreman", 003),
+       ("Will", "Foreman", 005),
+       ("Leo", "Testa", 007),
+       ("Connor", "Harris", 002),
+       ("Ethan", "Harris", 006),
+       ("Quentin", "Miller", 001),
+       ("Wes", "Miller", 004);
+
+UPDATE `employee_db`.`employee` SET `manager_id` = 1 WHERE (`id` > 1);
